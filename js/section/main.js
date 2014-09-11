983fe5da92ac53c6c88048a58293297ecc04c843
vtemian <vladtemian@gmail.com>
2014-09-11 20:36:40 +0300

    New version

--- js/section/main.js
+++
@@ -1,6 +0,0 @@
-$( function() {
-	$("header").html(headerTemplate());
-	$("header").append(menuTemplate({options: mainMenuOptions}));
-
-	$("footer").html(footerTemplate({options: footerMenuOptions}));
-});
